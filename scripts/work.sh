569b561428a0e8964ab644b17c12a6e2587b2b5a
Yoni Kosiner <yonikosiner@Yonis-Mac-mini.local>
2021-04-14 23:17:07 +0100

    personal website redesign script for tmux

--- scripts/work.sh
+++ scripts/work.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 while [ "$#" -gt 0 ]; do
     curr=$1
     shift
@@ -13,7 +12,10 @@ while [ "$#" -gt 0 ]; do
         tmux neww -t mycookbook: -n frontend -d 'cd ~/work/mycookbook/frontend && npm start'
         ;;
     "-yk")
-        tmux new -t yonikosiner-com -d
+        tmux new -s yonikosiner-com -d
+        tmux new -t yonikosiner-com: -n watch -d 'cd ~/personal/yonikosiner.com && npm run watch'
+        tmux new -t yonikosiner-com: -n server -d 'cd ~/personal/yonikosiner.com && npm run server'
+        tmux new -t yonikosiner-com: -n frontend -d 'cd ~/personal/yonikosiner.com/frontend && npm start'
         ;;
     *) echo "Unavailable command... $curr"
     esac
